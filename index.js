4ea7ef5e68fb41c740cf448cbb2ffc602ed8b9ad
Hesam <102720155+Hesam-Dev@users.noreply.github.com>
2022-07-28 19:42:23 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav__ul");
+menu.addEventListener("click", function() {
+
+  nav.classList.toggle("active")
+
+})
